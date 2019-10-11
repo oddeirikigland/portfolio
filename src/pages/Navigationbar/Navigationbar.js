886b51a174d89c3e2ac1e22ed2c393b2c0fa2164
Odd Eirik Igland <oddeirik.igland@gmail.com>
2019-10-11 14:26:08 +0100

    adds navbar

---
+++ src/pages/Navigationbar/Navigationbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+export default class Navigationbar extends Component {
+  render() {
+    return (
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/strava">Segment Analyzer</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+    );
+  }
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Icons from '../../components/Icons/Icons';
+import Navigationbar from "../Navigationbar/Navigationbar";
 import './Home.css';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div id={'home'}>
+				<Navigationbar />
 				<div id={'container'}>
 					<Icons />
 				</div>
