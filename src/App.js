25097bbc6ad424f6d5f736e747c3521540edb8b7
Odd Eirik Igland <oddeirik.igland@gmail.com>
2019-12-11 21:27:06 +0000

    animation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import SegmentAnalyzer from "./pages/segmentAnalyzer/SegmentAnalyzer";
 import Form from "./pages/form/form";
+import Animation from "./pages/animation/animation";
 
 const fouroffour = () => (
   <div
@@ -25,6 +26,7 @@ export default class App extends Component {
           <Route exact path={'/'} component={Home} />
           <Route exact path={'/strava'} component={SegmentAnalyzer} />
           <Route exact path={'/form'} component={Form} />
+          <Route exact path={'/animation'} component={Animation} />
           <Route component={fouroffour} />
         </Switch>
       </BrowserRouter>
