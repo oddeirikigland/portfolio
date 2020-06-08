2b5dc118f4fc2449e6ff9bfde614bf6c32c43c5a
oddeirikigland <oddeirikigland@users.noreply.github.com>
2020-06-08 16:37:19 +0000

    deploy: 031e4318f3beb7598858670d4b3c4e960d2392f1

---
+++ precache-manifest.849621b27c928aa88a051dd4eb3b42d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c47c08a32ef05550a55761e5fb965d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6e7743b816091451fc24",
+    "url": "/portfolio/static/css/2.94592c3b.chunk.css"
+  },
+  {
+    "revision": "d769adeead502d9dd822",
+    "url": "/portfolio/static/css/main.1ec96b62.chunk.css"
+  },
+  {
+    "revision": "6e7743b816091451fc24",
+    "url": "/portfolio/static/js/2.ff518674.chunk.js"
+  },
+  {
+    "revision": "f40658e6128396882ee070770f5a6e7f",
+    "url": "/portfolio/static/js/2.ff518674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d769adeead502d9dd822",
+    "url": "/portfolio/static/js/main.222143eb.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "d6f5fffe2f6c3a272a3cf9281971c0dd",
+    "url": "/portfolio/static/media/backlit.d6f5fffe.jpg"
+  }
+]);
