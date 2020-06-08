5b6fdd16e1fb9cc3fa09118cf3ca8f5294b198de
Odd Eirik Igland <oddeirik.igland@gmail.com>
2020-06-08 16:40:42 +0200

    dotenv config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { config } from 'dotenv';
+
+config()
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
